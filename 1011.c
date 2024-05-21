27c6e23db17be9374a45d52d0825bcafe203968a
fahimsafaat <140727330+Fahim-safaat@users.noreply.github.com>
2024-05-22 02:32:07 +0600

    Update 1011.c

--- 1011.c
+++ 1011.c
@@ -6,7 +6,7 @@ int main() {
     double VOLUME, r, pi = 3.14159, a = 4.0, b = 3.0;
      scanf("%lf", &r);
 
-     VOLUME = (a/b) * pi * (r*r*r);
+     VOLUME = (a/b) * pi * pow(r,3);
 
      printf("VOLUME = %.3lf\n", VOLUME);
 
